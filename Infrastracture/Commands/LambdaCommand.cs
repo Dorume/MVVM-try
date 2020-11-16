550d46d61e9c3afb755d282ad107a851f935993f
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-16 18:30:19 +0200

    Добавьте файлы проекта.

---
+++ Infrastracture/Commands/LambdaCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Program.Infrastracture.Commands.Base;
+
+namespace Program.Infrastracture.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<Object> execute, Func<Object, bool> canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Program.Infrastracture.Commands;
+using Program.ViewModels.Base;
+
+namespace Program.ViewModels
+{
+    class MainViewModel : ViewModel
+    {
+        #region Заголовок Окна
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        private string _Title = "Program";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Комманды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get;}
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+        #endregion
+
+        public MainViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            
+
+            #endregion
+        }
+
+    }
+}
